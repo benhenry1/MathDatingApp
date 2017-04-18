96a909bc362252f950e9d2f59047cc2e12199e87
Ben Henry <Ben Henry>
2017-04-18 16:45:37 -0400

    Added both algorithms to common

---
+++ common/src/main/java/com/example/DPAlgorithm.java
@@ -0,0 +1,134 @@
+package ChoiceImpl;
+import java.util.*;
+public class DPAlgorithm {
+	private double[] candidates;
+	private int numCandidates;
+
+	private double[] c;
+	private int[] s;
+	//private int[] rr;
+
+	private int optimal;
+
+	private SortedSet<Double> rankFinder;
+
+	public DPAlgorithm(int numCandidates) {
+		this.numCandidates = numCandidates;
+		this.candidates = generateCandidates(numCandidates); //generate candidates
+		//this.rr 		= generateRelativeRank(this.candidates); //populate the relative ranks y_i
+		this.c 			= generateCValues();
+		this.optimal    = getOptimalValue();
+
+		printStatistics();
+
+		//Debug from here out
+		/*String cs = "";String ss = ""; String rrs = ""; String candidatess = "";
+		for (int i = 0; i < numCandidates; i++) {
+			cs += c[i] + " ";
+			ss += s[i] + " ";
+			candidatess += candidates[i] + " ";
+		}
+		System.out.println("ALGORITHM COMPLETE. Results:");
+		System.out.println("Candidates: " + candidatess);
+		System.out.println("RR: " + rrs);
+		System.out.println("S[i]: " + ss);
+		System.out.println("C[i]: " + cs);
+		System.out.println("");
+		System.out.println("Calculated Optimal: " + this.optimal);*/
+	}
+	
+	public DPAlgorithm(double[] candidates) {
+		this.numCandidates = candidates.length;
+		this.candidates = candidates;
+		this.c = generateCValues();
+		this.optimal = getOptimalValue();
+		
+		printStatistics();
+	}
+
+	private double[] generateCandidates(int numCandidates) {
+		double[] ret = new double[numCandidates];
+
+		for (int i = 0; i < numCandidates; i++)
+			ret[i] = Math.random();
+
+		return ret;
+	}
+
+	private int[] generateRelativeRank(double[] cand) {
+		SortedSet<Double> seen = new TreeSet<Double>();
+		int[] relRank = new int[numCandidates];
+
+		for (int i = 0; i < cand.length; i++) {
+			seen.add(cand[i]);
+			relRank[i] = seen.tailSet(cand[i]).size();
+		}
+		return relRank;
+	}
+
+	//Populates s[] and returns c[]
+	private double[] generateCValues() {
+		double[] tentCVal = new double[numCandidates];
+		tentCVal[numCandidates - 1] = (numCandidates + 1) / 2;
+
+		this.s = new int[numCandidates];
+		s[numCandidates - 1] = (int)((numCandidates + 1) / 2); //init with s_n-1
+
+		for (int i = numCandidates - 2; i >= 0; i--) { //n-2 -> 0
+			//System.out.println("Calculating c[" + i + "]");
+			//get ci
+			double oneoveri = 1.0 / (i + 1.0);
+			double ratio = (numCandidates + 1.0) / (i + 2.0);
+			double t2 = (s[i+1] * (s[i+1] + 1.0)) / 2.0;
+			double lastadd = ((i + 1.0) - s[i+1]) * tentCVal[i+1];
+			double ci = oneoveri * (ratio * t2 + lastadd);
+			tentCVal[i] = ci;
+			//get si
+			int si = (int) (((i+(1.0))/(numCandidates+(1.0))) * ci);
+			s[i] = si;
+			//System.out.println(tentCVal[i] + " " + s[i]);
+		}
+		return tentCVal;
+	}
+
+	//Returns which i satisfies rr[i] < s[i], or -1 if not found.
+	//Even when the optimal I is found, it continues to fully create the sorted set. Used for stats later
+	private int getOptimalValue() {
+		SortedSet<Double> seen = new TreeSet<Double>();
+		int[] relRank = new int[numCandidates];
+		boolean continueLooking = true;
+		int optimalI = -1;
+
+		for (int i = 0; i < numCandidates; i++) {
+			seen.add(candidates[i]);
+			relRank[i] = seen.tailSet(candidates[i]).size();
+			if (relRank[i] < s[i] & continueLooking) {
+				optimalI = i;
+				continueLooking = false;
+			}
+		}
+		this.rankFinder = seen;
+		return optimalI;
+	}
+
+	private void printStatistics() {
+		//Tell them their input, chosen stopping point, chosen candidate, and the rank of the candidate.
+		String str = "";
+		str+= "Based on your input of size " + numCandidates + ", the algorithm determined the optimal stopping point was at candidate " + this.optimal + ".\n";
+		str+= "Using this optimal solution, the algorithm chose the value " + candidates[optimal] + ", which is of rank " + rankFinder.tailSet(candidates[optimal]).size() + " in the total set of candidates.\n"; 
+		System.out.println(str);
+		System.out.println(c[0]);
+	}
+	
+	public double getChosenValue() {
+		return candidates[optimal];
+	}
+	public double getChosenRank() {
+		return rankFinder.tailSet(candidates[optimal]).size();
+	}
+
+
+	public static void main(String[] args) {
+		new DPAlgorithm(100);
+	}
+}
---
+++ common/src/main/java/com/example/MathOfChoice.java
@@ -0,0 +1,170 @@
+package ChoiceImpl;
+import java.util.Arrays;
+//import java.util.ArrayUtils;
+
+public class MathOfChoice {
+	//set by generateCandidates(int)
+	private double[] candidates = null;
+
+
+	//set by calculateOptimal()
+	private double chanceOfMaxRank = 0;
+	public int    optimalR = 0;
+
+	//set by optimalChoiceAlgorithm()
+	public double maxEltFound = 0;
+	public int    maxEltFoundIndex = 0;
+	private double maxEltFoundWithinR = 0;
+	private int    maxEltFoundIndexWithinR = 0;
+
+
+	public MathOfChoice(int numCandidates) {
+		if (numCandidates <= 0) {
+			System.out.println("Must have at least 1 candidate.");
+			return;
+		}
+		calculateOptimal(numCandidates); 					//calculate optimal R value to stop searching
+		this.candidates = generateCandidates(numCandidates);//Generate a list of random numbers of the desired size
+
+		if (candidates != null )
+			optimalChoiceAlgorithm(optimalR);	//assuming that there are candidates,
+		else
+			throw new IllegalStateException("Something went wrong when creating the candidate list.");
+
+		printStatistics();
+	}
+	
+	public MathOfChoice(double[] candidates) {
+		if (candidates == null) {
+			System.out.println("Must have at least 1 candidate.");
+			return;
+		}
+		calculateOptimal(candidates.length); 					//calculate optimal R value to stop searching
+		this.candidates = candidates;//Generate a list of random numbers of the desired size
+
+		//if (candidates != null )
+			optimalChoiceAlgorithm(optimalR);	//assuming that there are candidates,
+		//else
+		//	throw new IllegalStateException("Something went wrong when creating the candidate list.");
+
+		printStatistics();
+	}
+
+
+	private double[] generateCandidates(int numCandidates) {
+		double[] ans = new double[numCandidates];
+
+		for (int i = 0; i < numCandidates; i++) {
+			ans[i] = Math.random();
+		}
+		return ans;
+	}
+
+	private void calculateOptimal(int numCandidates) {
+		double maxChance = 0;
+		int maxR = 0;
+		int n = numCandidates;
+
+		for(int r = 2; r <= n; r++) {
+			double thisChance = 0;
+			double mul = (double) (r - 1) / n;
+			double sum = 0;
+			for (int j = r; j <= n; j++) {
+				sum += (double)Math.pow((j-1), -1);
+			}
+			thisChance = mul * sum;
+			/*System.out.println(numCandidates + ": " + thisChance + " = " + mul + " * " + sum);*/
+			if (thisChance > maxChance) {
+				maxChance = thisChance;
+				maxR = r;
+			}
+		}
+		this.optimalR = maxR;
+		this.chanceOfMaxRank = maxChance;
+		System.out.println(maxChance + "% at r = " + maxR);
+		//return maxChance;
+	}
+
+	private void optimalChoiceAlgorithm(int optimalR) {
+		/*System.out.println("*************************OPTCHOICE***********************************************");*/
+		int counter = 0;
+		double currOptimal = -1;
+		int posOfCurrOptimal = 0;
+
+		double optBeforeR = -1;
+		int posBeforeR = 0;
+
+		while (counter < candidates.length) {
+			/*System.out.println("Debug: " + counter + " " + optimalR);*/
+				if (counter > optimalR) {
+						if (candidates[counter] >= optBeforeR) {
+							currOptimal = candidates[counter];
+							posOfCurrOptimal = counter;
+							break;
+						} else {
+							//we are past optimal threshold but did not find a maximum. do nothing.
+						}
+				} else {
+						if (candidates[counter] > optBeforeR) {
+								optBeforeR = candidates[counter];
+								posBeforeR = counter;
+							
+						} else {
+							//Not past optimalR and this isn't a local max. do nothing
+						}
+				}
+
+			
+			counter++;
+		}
+
+		System.out.println(counter + ": " + currOptimal + " " + optBeforeR + " index " + posOfCurrOptimal + " " + posBeforeR);
+		
+		if (currOptimal > 0) {
+			this.maxEltFound =  currOptimal;
+			this.maxEltFoundIndex = posOfCurrOptimal;
+		} else {
+			this.maxEltFound = candidates[candidates.length - 1];
+			this.maxEltFoundIndex = candidates.length - 1;
+		}
+		this.maxEltFoundWithinR = optBeforeR;
+		this.maxEltFoundIndexWithinR = posBeforeR;
+		//return posOfCurrOptimal;
+	}
+
+	private void printStatistics() {
+		String stats = "";
+		int rankOfAnswer = getRankOf(maxEltFoundIndex, candidates);
+		int rankOfElementBeforeR = getRankOf(maxEltFoundIndexWithinR, candidates);
+		stats += "For this randomly generated " + candidates.length + "-element set, the algorithm determined the optimal place to stop evaluating was at element " + optimalR + ".\n";
+		stats += "Before reaching " + optimalR + ", the largest value found was " + maxEltFoundWithinR + " at index " + maxEltFoundIndexWithinR + ". This element was of rank " + rankOfElementBeforeR + "\n";
+		stats += "Once element " + optimalR + " was passed, the next largest element found was " + maxEltFound + " at index " + maxEltFoundIndex + ", which was the final choice of the algorithm.\n\n";
+		stats += "The chosen element " + maxEltFound + " was of rank " + rankOfAnswer + " in the original set of candidates.\n";
+
+
+		System.out.println(stats);
+
+	}
+
+	private int getRankOf(int index, double[] array) {
+		if (index > array.length)
+			return -1;
+		double value = array[index];
+		double[] manip = Arrays.copyOf(array, array.length);
+		Arrays.sort(manip);
+
+		for (int i = candidates.length - 1; i > 0; i--) {
+			if (manip[i] == value) {
+				return candidates.length - i;
+			}
+		}
+
+		//The value was not found in the sorted array.....
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		new MathOfChoice(1000);
+	}
+
+}
